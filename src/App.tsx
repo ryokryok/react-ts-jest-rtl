dc89a03e277bbefe3fe6c89b83041d4a138da104
ryokryok <mrozisan@gmail.com>
2020-08-23 22:20:36 +0900

    write App.tsx test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+function useAuth() {
+  const [auth, setAuth] = useState(false);
+  function handleClick() {
+    setAuth(!auth);
+  }
+  return { auth, handleClick };
+}
 
 export default function App() {
+  const { auth, handleClick } = useAuth();
   return (
     <div>
-      <h1>This is React with TypeScript</h1>
+      {auth ? (
+        <>
+          <h1>Welcome</h1>
+          <button name="signOut" onClick={handleClick}>
+            Sign out
+          </button>
+        </>
+      ) : (
+        <>
+          <h1>Please sign in</h1>
+          <button name="signIn" onClick={handleClick}>
+            Sign in
+          </button>
+        </>
+      )}
     </div>
   );
 }
